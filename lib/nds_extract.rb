7adc78d66c72506bb57a957a13de6427730d39e6
david v perekopskiy <david.perekopskiy@go.shoreline.edu>
2020-01-17 06:14:37 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,9 +3,11 @@ require 'directors_database'
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data, column_index)
+def gross_for_director(director_data)
 total=0 
+column_index=0
 total+=director_data[:movies][column_index][:worldwide_grosses]
+column_index+=1
 total
 end
 
